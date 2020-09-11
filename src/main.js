6262285be689b8f9b46e93c714ad7984a01d1a73
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-09-11 09:30:49 -0700

    adding UI logic to main.js

--- src/main.js
+++ src/main.js
@@ -5,4 +5,10 @@ import './css/styles.css';
 import CurrencyExchange from './../js/currency-service.js';
 import CurrencyConversion from './../js/currency-logic.js';
 
-
+$(document).ready(function () {
+  $("#exchangeButton").click(function () {
+    const currencyAmount = parseInt($("#currency")).val();
+    const exchangeFrom = $("#currencyType").val();
+    const exchangeTo = $("#currencyType2").val();
+  })
+})
