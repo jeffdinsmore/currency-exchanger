3272bd52ad2f751a0e7e76a010112cfa8a9a4e06
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-09-11 11:33:13 -0700

    attempting to write jquery in main.js to dynamically get the value of the promise from the api by using the user input field

--- src/main.js
+++ src/main.js
@@ -10,13 +10,28 @@ $(document).ready(function () {
     // const currencyAmount = parseInt($("#currency")).val();
     const exchangeFrom = $("#currencyType").val();
     console.log(exchangeFrom);
-    // const exchangeTo = $("#currencyType2").val();
+    const exchangeTo = $("#currencyType2").val();
     $("#currency").val("");
     let promise = CurrencyExchange.getCurrency(exchangeFrom);
     promise.then(function(response) {
+
       const main = JSON.parse(response);
-      $("#currencyOutput1").text(main.conversion_rates.USD);
-      $("#currencyOutput2").text(main.conversion_rates.AED);
+      const USD = main.conversion_rates.USD;
+      const AED = main.conversion_rates.AED;
+      const ARS = main.conversion_rates.ARS;
+      const AUD = main.conversion_rates.AUD;
+      const BGN = main.conversion_rates.BGN;
+      const BRL = main.conversion_rates.BRL;
+      const BSD = main.conversion_rates.BSD;
+      console.log(USD);
+      console.log(AED);
+      console.log(ARS);
+      console.log(AUD);
+      console.log(BGN);
+      console.log(BRL);
+      console.log(BSD);
+      $("#currencyOutput1").attr(exchangeFrom);
+      $("#currencyOutput2").text(exchangeTo);
     }, function(error) {
       $("#outputError").html(`Error! Please try again ${error}`);
     });
