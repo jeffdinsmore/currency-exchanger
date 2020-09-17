19611b1be4ac7035c80ebb5d14a6eba974876e75
JeffDinsmore <hello@jeffdinsmore.com>
2020-09-16 21:47:31 -0700

    relabeled some variables for easier reading. Added extra error handling on line 14-17 of main.js if someone inputs nothing for currency type. Took out the remaining console.logs

--- src/main.js
+++ src/main.js
@@ -7,27 +7,31 @@ import CurrencyExchange from './currency-service.js';
 $(document).ready(function () {
   $("#executeButton").click(function () {
     const currencyAmount = parseFloat($("#currency").val()).toFixed(2);
-    let exchangeFromInput = $("#currencyType").val();
-    let exchangeToInput = $("#currencyType2").val();
+    let firstCurrencyTypeInput = $("#currencyType1").val();
+    let secondCurrencyTypeInput = $("#currencyType2").val();
     if (currencyAmount == "NaN") {
       alert("Please enter a number in the 'Enter An Amount' field.");
+    } else if (firstCurrencyTypeInput.length == 0 || secondCurrencyTypeInput.length == 0) {    
+      $("#outputErrorFromInput").show();
+      $("#outputRate").hide();
+      $("#result").hide();
     } else {
       $("#result").show();
       $("#outputErrorFromInput").hide();
       $("#outputRate").show();
       
-      let promise = CurrencyExchange.getCurrency(exchangeFromInput);
+      let promise = CurrencyExchange.getCurrency(firstCurrencyTypeInput);
       promise.then(function(response) {
         const apiResponse = JSON.parse(response);
-        if (apiResponse.result === "error" || apiResponse.conversion_rates[exchangeToInput] == null) {
+        if (apiResponse.result === "error" || apiResponse.conversion_rates[secondCurrencyTypeInput] == null || apiResponse.conversion_rates[firstCurrencyTypeInput] == null) {
           $("#outputErrorFromInput").show();
           $("#outputRate").hide();
           $("#result").hide();
         }
-        let firstCurrencyTypeInput = apiResponse.conversion_rates[exchangeFromInput];
-        let secondCurrencyTypeInput = apiResponse.conversion_rates[exchangeToInput];
-        $("#outputRate").text(firstCurrencyTypeInput + exchangeFromInput + " = " + secondCurrencyTypeInput + exchangeToInput);
-        $("#currencyExchangeOutput").text("You have " + (currencyAmount * firstCurrencyTypeInput).toFixed(2) + exchangeFromInput + " that equals " + (currencyAmount * secondCurrencyTypeInput).toFixed(2) + exchangeToInput + ".");
+        let firstCurrencyApiResponseForOutput = apiResponse.conversion_rates[firstCurrencyTypeInput];
+        let secondCurrencyApiResponseForOutput = apiResponse.conversion_rates[secondCurrencyTypeInput];
+        $("#outputRate").text(firstCurrencyApiResponseForOutput + firstCurrencyTypeInput + " = " + secondCurrencyApiResponseForOutput + secondCurrencyTypeInput);
+        $("#currencyExchangeOutput").text("You have " + (currencyAmount * firstCurrencyApiResponseForOutput).toFixed(2) + firstCurrencyTypeInput + " that equals " + (currencyAmount * secondCurrencyApiResponseForOutput).toFixed(2) + secondCurrencyTypeInput + ".");
       }, function(error) {
         $("#outputError").html(`There was an error processing your request: ${error}`);
       });
